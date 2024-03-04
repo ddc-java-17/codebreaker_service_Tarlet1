98465d2389064fe52b9408e0e747debc26890603
Timothy Arlet <tarlet@cnm.edu>
2024-03-04 15:53:45 -0700

    Start of many to many.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -12,12 +12,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.persistence.UniqueConstraint;
 import java.time.Instant;
 import java.util.LinkedList;
 import java.util.List;
@@ -75,6 +79,25 @@ public class User {
   @JsonIgnore
   private final List<Game> games = new LinkedList<>();
 
+  @ManyToMany(fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "user_following",
+      uniqueConstraints = @UniqueConstraint(columnNames = {"followed_id", "follower_id"}),
+      joinColumns = @JoinColumn(name = "followed_id", nullable = false),
+      inverseJoinColumns = @JoinColumn(name = "follower_id", nullable = false)
+  )
+  @OrderBy("displayName")
+  @JsonIgnore
+  private final List<User> followedUsers = new LinkedList<>();
+
+  @ManyToMany(mappedBy = "followedUsers",
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+  )
+  @OrderBy("displayName")
+  @JsonIgnore
+  private final List<User> followingUsers = new LinkedList<>();
+
   @NonNull
   public Long getId() {
     return id;
@@ -93,6 +116,14 @@ public class User {
   @NonNull
   public Instant getModified() {
     return modified;
+  }
+
+  public List<User> getFollowedUsers() {
+    return followedUsers;
+  }
+
+  public List<User> getFollowingUsers() {
+    return followingUsers;
   }
 
   @NonNull
